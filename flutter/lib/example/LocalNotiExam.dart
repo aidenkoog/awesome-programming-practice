4047b70f71acb26974a9c0206a5ea411ab3d2c00
AidenKoo <k037686@naver.com>
2023-04-12 18:29:20 +0900

    feat: add package for flutter local notifications

---
+++ flutter/lib/example/LocalNotiExam.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LocalNotiExam extends StatefulWidget {
+  const LocalNotiExam({super.key});
+
+  @override
+  State<LocalNotiExam> createState() => _LocalNotiExamState();
+}
+
+class _LocalNotiExamState extends State<LocalNotiExam> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
