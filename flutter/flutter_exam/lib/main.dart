e43548fa77da2f1022eceb5c7c5e710415d5a583
AidenKoo <k037686@naver.com>
2023-04-12 16:41:17 +0900

    [feat.] Add flutter toast example

--- flutter/flutter_exam/lib/main.dart
+++ flutter/flutter_exam/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_exam/page/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: 
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomePage());
   }
 }
