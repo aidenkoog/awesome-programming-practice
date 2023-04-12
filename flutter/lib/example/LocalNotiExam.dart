1dcbcc648a23abcc1ea034f6331680d216f92be1
AidenKoo <k037686@naver.com>
2023-04-12 19:07:03 +0900

    feat: add notification detail setting & permission setting

--- flutter/lib/example/LocalNotiExam.dart
+++ flutter/lib/example/LocalNotiExam.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+import '../main.dart';
 
 class LocalNotiExam extends StatefulWidget {
   const LocalNotiExam({super.key});
@@ -8,8 +12,52 @@ class LocalNotiExam extends StatefulWidget {
 }
 
 class _LocalNotiExamState extends State<LocalNotiExam> {
+  // build method is called whenever screen change is detected.
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+              onPressed: () async {
+                final notification = flutterLocalNotificationsPlugin;
+
+                // Android notification setting
+                const android = AndroidNotificationDetails(
+                  '0',
+                  'Alarm Test',
+                  channelDescription: 'Alarm Test Body',
+                  importance: Importance.max,
+                  priority: Priority.max,
+                );
+
+                // iOS notification setting
+                const ios = DarwinNotificationDetails();
+                // ignore: unused_local_variable
+                const detail = NotificationDetails(
+                  android: android,
+                  iOS: ios,
+                );
+
+                // permission Android always true
+                final permission = Platform.isAndroid
+                    ? true
+                    : await notification
+                            .resolvePlatformSpecificImplementation<
+                                IOSFlutterLocalNotificationsPlugin>()
+                            ?.requestPermissions(
+                                alert: true, badge: true, sound: true) ??
+                        false;
+                // ignore: unnecessary_brace_in_string_interps, avoid_print
+                print('permission ${permission}');
+              },
+              child: const Text('Add alarm')),
+          const Center(child: Text('Hello2'))
+        ],
+      ),
+    );
   }
 }
--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,8 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_exam/example/LocalNotiExam.dart';
 import 'package:flutter_exam/example/ToastExam.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  _initNotiSetting();
   runApp(const MyApp());
+}
+
+final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+void _initNotiSetting() async {
+  // Android
+  const initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  // iOS
+  const initializationSettingsIOS = DarwinInitializationSettings(
+    requestAlertPermission: true,
+    requestBadgePermission: true,
+    requestSoundPermission: true,
+  );
+
+  // initial settings
+  const initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsIOS,
+  );
+
+  final result = await flutterLocalNotificationsPlugin.initialize(
+    initializationSettings,
+  );
+  print(result);
 }
 
 class MyApp extends StatelessWidget {
