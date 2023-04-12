36e5bb42e1f2a8c06c152b1dcba154249a6b0cfc
AidenKoo <k037686@naver.com>
2023-04-12 11:45:12 +0900

    [Feature] Upload flutter example intial source code

---
+++ flutter/flutter_exam/lib/page/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+              onPressed: () {},
+              child: const Center(child: Text('Hello AidenKooG'))),
+        ],
+      ),
+    );
+  }
+}
