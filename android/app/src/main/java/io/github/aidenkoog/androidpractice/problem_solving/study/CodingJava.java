8b7529bb9f36a1c1a45d0f68917dd009f5e82d24
AidenKooG <k037686@gmail.com>
2023-11-21 00:11:51 +0900

    feat: add string related example source code

--- android/app/src/main/java/io/github/aidenkoog/androidpractice/problem_solving/study/CodingJava.java
+++ android/app/src/main/java/io/github/aidenkoog/androidpractice/problem_solving/study/CodingJava.java
@@ -1,4 +1,34 @@
 package io.github.aidenkoog.androidpractice.problem_solving.study;
 
 public class CodingJava {
+    public static void main(String[] args) {
+        System.out.println("test");
+
+        // java.lang.String
+    }
+
+    public static void practice() {
+        // String
+        String str = "adsdfsdfs";
+        str = "sdfhsdkfjshdkf";
+
+        char[] tempArr = str.toCharArray();
+        tempArr[4] = 'R';
+        str = new String(tempArr);
+        System.out.println(str);
+
+        String newStr = str.substring(0, 4) + "R" + str.substring(4, 7);
+
+        // constant String pool. (Java Heap 내 따로 공간이 있음)
+        // strLiteral과 2는 같음 (==)
+        String strLiteral = "test";
+        String strLiteral2 = "test";
+
+        // Java Heap.
+        String strObject = new String("test");
+
+        // 스트링 메소드
+        // charAt / length / equals / compareTo / toCharArray / contains / replace / split /
+        // substring / indexOf (문자의 위치를 반환) / toLowerCase / toUpperCase
+    }
 }
