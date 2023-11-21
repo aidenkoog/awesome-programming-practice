6b680be6fe05038f02f1a0b9f95aeaea11c60f01
AidenKooG <k037686@gmail.com>
2023-11-21 23:30:22 +0900

    feat: add how to find target number using 2 pointers

--- android/app/src/main/java/io/github/aidenkoog/androidpractice/problem_solving/study/CodingKotlin.kt
+++ android/app/src/main/java/io/github/aidenkoog/androidpractice/problem_solving/study/CodingKotlin.kt
@@ -1,4 +1,23 @@
 package io.github.aidenkoog.androidpractice.problem_solving.study
 
 class CodingKotlin {
+    fun twoSum(nums: IntArray, target: Int): Boolean {
+
+        // 일단 정렬, 빅오 n
+        nums.sort()
+
+        var l = 0
+        var r = 0
+
+        while (l < r) {
+            if (nums[l] + nums[r] > target) {
+                r -= l
+            } else if (nums[l] + nums[r] < target) {
+                l += 1
+            } else if (nums[l] + nums[r] == target) {
+                return true
+            }
+        }
+        return false
+    }
 }
