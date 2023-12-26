dcffe4a30c25ed4dcc6b8ac8c3f5c4ab75e4b82a
AidenKooG <k037686@gmail.com>
2023-12-26 20:39:12 +0900

    feat: add observer example classes

--- android/app/src/main/java/io/github/aidenkoog/androidpractice/design_pattern/Observer.java
+++ android/app/src/main/java/io/github/aidenkoog/androidpractice/design_pattern/Observer.java
@@ -1,4 +1,66 @@
 package io.github.aidenkoog.androidpractice.design_pattern;
 
-public class Observer {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+interface Observer {
+    void update(NumberGenerator generator);
+}
+
+class DigitObserver implements Observer {
+
+    @Override
+    public void update(NumberGenerator generator) {
+        System.out.println("DigitObserver");
+    }
+}
+
+abstract class NumberGenerator {
+    private List<Observer> observers = new ArrayList<>();
+
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    public void notifyObservers() {
+        Iterator<Observer> ir = observers.iterator();
+
+        while (ir.hasNext()) {
+            Observer observer = ir.next();
+            observer.update(this);
+        }
+    }
+
+    public abstract int getNumber();
+
+    public abstract void execute();
+}
+
+class RandomNumberGenerator extends NumberGenerator {
+    private Random random = new Random();
+
+    @Override
+    public int getNumber() {
+        return 0;
+    }
+
+    @Override
+    public void execute() {
+        System.out.println("execute!");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        NumberGenerator generator = new RandomNumberGenerator();
+        Observer observer = new DigitObserver();
+        generator.addObserver(observer);
+        generator.execute();
+    }
 }
