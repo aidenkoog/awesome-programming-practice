8e114d711872dda1e1d9d0107fb596967df71662
AidenKooG <k037686@gmail.com>
2023-12-26 23:18:25 +0900

    feat: add iterator and mediator design patterns description

---
+++ android/app/src/main/java/io/github/aidenkoog/androidpractice/design_pattern/Iterator.java
@@ -0,0 +1,8 @@
+package io.github.aidenkoog.androidpractice.design_pattern;
+
+/*
+ * 객체 내부에 접근해서 순회하는 것이 아닌 외부에서 객체에 순회하는 객체를 생성
+ * Collections 에 대해 iterator() 후 반환되는 Iterator 를 받아서 내부 순회.
+ */
+public class Iterator {
+}
--- android/app/src/main/java/io/github/aidenkoog/androidpractice/design_pattern/Mediator.java
+++ android/app/src/main/java/io/github/aidenkoog/androidpractice/design_pattern/Mediator.java
@@ -1,4 +1,11 @@
 package io.github.aidenkoog.androidpractice.design_pattern;
 
+/*
+ * 카운셀러, 중재자 패턴
+ * UI 프로그래밍 때 자주 사용됨.
+ * 위젯 간의 상호 처리를 서로 간에 처리하는 것이 아닌 한 객체가 전담하여 처리하도록 하는 방식
+ * N:N 관계를 1:N의 관계로 변경 가능
+ * 중앙에서 컨트롤 한다는 개념, 유지 보수 수월.
+ */
 public class Mediator {
 }
