4cfe4949de7cd1418811f4ef9a963c17554a7fa4
AidenKooG <k037686@gmail.com>
2023-12-26 23:53:47 +0900

    feat: add concept of Chain of responsibility

---
+++ android/app/src/main/java/io/github/aidenkoog/androidpractice/design_pattern/ChainOfResponsibility.java
@@ -0,0 +1,11 @@
+package io.github.aidenkoog.androidpractice.design_pattern;
+
+/*
+ * 책임 떠넘기기
+ * 요청을 해결할 객체를 만날 때까지 객체 고리를 따라서 요청을 전달
+ * 요청의 근원지는 모를 수 있음
+ * 객체의 책임을 추가 / 변경 / 확장 가능
+ * ex. object.setNext(..).setNext(..)... / object.support(new Problem(100))
+ */
+public class ChainOfResponsibility {
+}
