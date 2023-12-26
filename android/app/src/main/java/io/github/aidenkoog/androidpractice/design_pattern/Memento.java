de14ae06a28b3c540745660cb8e381238253c815
AidenKooG <k037686@gmail.com>
2023-12-26 20:57:21 +0900

    feat: add observer example classes

--- android/app/src/main/java/io/github/aidenkoog/androidpractice/design_pattern/Memento.java
+++ android/app/src/main/java/io/github/aidenkoog/androidpractice/design_pattern/Memento.java
@@ -1,4 +1,11 @@
 package io.github.aidenkoog.androidpractice.design_pattern;
 
+/*
+ * 히스토리 보관하는 패턴 (객체의 내부 상태를 보관하는 패턴)
+ * - 인스턴스의 값들을 보관한다는 의미
+ * - 복원을 하기 위해 보관한다는 의미
+ * - Undo, Redo, 기억 순간을 저장하는 개체, 오류 복구 및 수행 결과 취소하기 위한 작업에 사용
+ * - Originator: User, Gamer etc.
+ */
 public class Memento {
 }
