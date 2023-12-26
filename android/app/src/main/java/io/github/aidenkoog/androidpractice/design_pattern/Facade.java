be7045db2df0e4df770b2ad97f9d98d029d22c28
AidenKooG <k037686@gmail.com>
2023-12-26 21:01:24 +0900

    feat: add description of facade design pattern

---
+++ android/app/src/main/java/io/github/aidenkoog/androidpractice/design_pattern/Facade.java
@@ -0,0 +1,11 @@
+package io.github.aidenkoog.androidpractice.design_pattern;
+
+/*
+ * 간단한 창구
+ * 서브 시스템을 더 편하게 이용하기 위한 높은 수준의 인터페이스 제공
+ * 컴파일러 (하나의 창구)
+ * 우리가 서브 시스템 즉, 컴파일러 내부 과정을 알 필요가 없음.
+ * 클라이언트와 서브시스템간의 결함도 감소 목적
+ */
+public class Facade {
+}
