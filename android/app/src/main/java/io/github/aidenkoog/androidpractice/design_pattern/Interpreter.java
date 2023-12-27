d8c733d7cf7bbe052e2f07dc51cbdb4fa480bf23
AidenKooG <k037686@gmail.com>
2023-12-27 20:31:08 +0900

    feat: add interpreter and prototype patterns

---
+++ android/app/src/main/java/io/github/aidenkoog/androidpractice/design_pattern/Interpreter.java
@@ -0,0 +1,12 @@
+package io.github.aidenkoog.androidpractice.design_pattern;
+
+/*
+ * 문법 규칙을 클래스로 표현
+ * 인터프리터 언어 (스크립트 언어), 완성되는 기계어 코드 없음
+ * 예. 페이지 로딩되면서 해석
+ * 문법을 해석하는 해석기를 만드는 것이 인터프리터 패턴
+ * Chain Of Responsibility와 유사
+ * 클래스와 Parsing 하는 부분으로 나뉨
+ */
+public class Interpreter {
+}
