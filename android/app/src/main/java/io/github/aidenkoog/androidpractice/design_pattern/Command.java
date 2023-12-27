0f407f2039a9171c6fba4f7de2edf46de4ebd652
AidenKooG <k037686@gmail.com>
2023-12-27 20:10:31 +0900

    feat: add command pattern description

---
+++ android/app/src/main/java/io/github/aidenkoog/androidpractice/design_pattern/Command.java
@@ -0,0 +1,14 @@
+package io.github.aidenkoog.androidpractice.design_pattern;
+
+/*
+ * 명령을 클래스로 제작
+ * - execute()
+ * Macro 커맨드
+ * - Stack 사용한 처리
+ */
+class CommandMain {
+}
+
+interface Command {
+    void execute();
+}
