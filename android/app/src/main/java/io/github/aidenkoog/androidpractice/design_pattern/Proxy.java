3dc7bea154ad2b14fca834394c8888c8adfdd945
AidenKooG <k037686@gmail.com>
2023-12-27 20:05:26 +0900

    feat: add proxy pattern description

--- android/app/src/main/java/io/github/aidenkoog/androidpractice/design_pattern/Proxy.java
+++ android/app/src/main/java/io/github/aidenkoog/androidpractice/design_pattern/Proxy.java
@@ -1,4 +1,27 @@
 package io.github.aidenkoog.androidpractice.design_pattern;
 
+/*
+ * 객체에 대한 접근을 제어하기 위해 대리자를 둠
+ */
 public class Proxy {
+}
+
+interface Printable {
+    String getPrinterName();
+}
+
+class PrinterProxy {
+
+}
+
+class Printer {
+    String name;
+
+    public Printer() {
+
+    }
+
+    public Printer(String name) {
+        this.name = name;
+    }
 }
