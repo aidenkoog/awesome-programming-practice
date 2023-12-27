d0bcc9d9ef59357f523ccac11e17b1cf195151cf
AidenKooG <k037686@gmail.com>
2023-12-27 19:48:04 +0900

    feat: add flyweight and chain of responsibility pattern examples

--- android/app/src/main/java/io/github/aidenkoog/androidpractice/design_pattern/ChainOfResponsibility.java
+++ android/app/src/main/java/io/github/aidenkoog/androidpractice/design_pattern/ChainOfResponsibility.java
@@ -1,11 +1,54 @@
 package io.github.aidenkoog.androidpractice.design_pattern;
 
+import androidx.annotation.NonNull;
+
 /*
  * 책임 떠넘기기
  * 요청을 해결할 객체를 만날 때까지 객체 고리를 따라서 요청을 전달
  * 요청의 근원지는 모를 수 있음
  * 객체의 책임을 추가 / 변경 / 확장 가능
  * ex. object.setNext(..).setNext(..)... / object.support(new Problem(100))
+ * ex. Help 시스템 - 적절한 답을 찾을 때까지 연결되어 해결 가능
  */
-public class ChainOfResponsibility {
+class ChainOfResponsibility {
+
+}
+
+abstract class Support {
+    private String name;
+
+    private Support next;
+
+    public Support(String name) {
+        this.name = name;
+    }
+
+    // setter for next supporter.
+    public Support setNext(Support next) {
+        this.next = next;
+        return next;
+    }
+
+    public final void support(Object trouble) {
+        if (resolve(trouble)) {
+            done();
+        } else if (next != null) {
+            // support function is called using next object.
+            next.support(trouble);
+        } else {
+            fail();
+        }
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "[" + name + "]";
+    }
+
+    protected abstract boolean resolve(Object trouble);
+
+    abstract void done();
+
+    abstract void fail();
 }
---
+++ android/app/src/main/java/io/github/aidenkoog/androidpractice/design_pattern/FlyWeight.java
@@ -0,0 +1,10 @@
+package io.github.aidenkoog.androidpractice.design_pattern;
+
+/*
+ * 공유할 수 있는 객체는 공유하여 사용
+ * 작은 여러개의 객체를 관리해야 할 때 주로 사용
+ * 싱글턴으로 만들어진 객체들이 여러개 존재, 풀에서 관리되고 있다 정도로 이해
+ * 각 객체를 판별할 수 있는 키 값 필요
+ */
+public class FlyWeight {
+}
